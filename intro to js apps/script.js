f1004ba8bb56bb64598c889988e1abea74309ac3
erica143-jinnie <afrozashimu275@gmail.com>
2024-10-04 21:27:35 +0600

    index

---
+++ intro to js apps/script.js
@@ -0,0 +1,24 @@
+var choice = prompt("Welcome to Area Calculator. \n Please Enter your Choice. \n1.Area of Rectangle. \n2.Area of Triangle. \n3.Area of Circle. \n4.Area of Parallelogram");
+if (choice=='1'){
+var 1 = prompt('enter the length')
+var b = prompt('enter breadth')
+var result = Number(1) * Number(b)
+alert ('The Area is '+ result)
+}
+if (choice == '2') {
+var h = prompt('enter the height')
+var b = prompt('enter base')
+var result = Number(h) * Number(b)/2
+alert('The Area is ' + result)
+}
+if (choice == '3') {
+var r = prompt('enter the radius')
+var result = 3.14*Number(r)*Number(r)
+alert('The Area is ' + result)
+}
+if (choice == '4') {
+var h = prompt('Enter the height')
+var cb = prompt('Enter the corresponding base')
+var result = Number(h) * Number(cb)
+alert('The Area is ' + result)
+}
