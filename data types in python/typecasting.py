511712233cba089f5ac2b3ff348a3047e5eabe9d
erica143-jinnie <afrozashimu275@gmail.com>
2024-12-06 21:36:33 +0600

    python operators

---
+++ data types in python/typecasting.py
@@ -0,0 +1,12 @@
+name = Penguin
+age = 15
+is_student = 38.5
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("age :", age)
+print("Data Type of age is", type(age))
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(name))
