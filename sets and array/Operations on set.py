ef6f94adf13ff448816dfe76f95c9cb0c07c0db6
erica143-jinnie <afrozashimu275@gmail.com>
2025-02-07 21:43:29 +0600

    sets array

---
+++ sets and array/Operations on set.py
@@ -0,0 +1,11 @@
+my_set ={1,2,3}
+print(my_set)
+my_set ={0.1,"hello",(1,2,3)}
+my_set = {1,2,3,4,3,2}
+print(my_set)
+my_set =set([1,2,3,2])
+print(my_set)
+my_set2 = set([0,1,3,4,5])
+print(my_set2)
+my_set2.pop()
+print(my_set2)
