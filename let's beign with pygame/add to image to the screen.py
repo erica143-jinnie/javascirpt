1c91a98299b32d736c99fdd2167ea7779182b0e1
erica143-jinnie <afrozashimu275@gmail.com>
2025-03-14 21:36:17 +0600

    pygame

---
+++ let's beign with pygame/add to image to the screen.py
@@ -0,0 +1,6 @@
+pygame.init()
+SCREEN_WIDTH,SCREEN_HEIGHT = 500,500
+display_surface = pygame.display.set_model((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Adding image and background image')
+
+background_image
