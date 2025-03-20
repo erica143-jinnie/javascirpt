76fc3e445f55f1f53c44a0aa02296344edd60ab3
erica143-jinnie <afrozashimu275@gmail.com>
2025-03-20 22:52:18 +0600

    rectriangle

--- let's beign with pygame/add to image to the screen.py
+++ let's beign with pygame/add to image to the screen.py
@@ -3,4 +3,4 @@ SCREEN_WIDTH,SCREEN_HEIGHT = 500,500
 display_surface = pygame.display.set_model((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Adding image and background image')
 
-background_image
+background_image = pygame.transformm.scale()
